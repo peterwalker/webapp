87cf10cbbcf908c22bbf86b0d1cb4cafd9995215
peterwalker <peterbwalker@me.com>
2012-10-08 08:51:50 -0400

    Findbugs

--- src/main/java/org/gradle/HelloServlet.java
+++ src/main/java/org/gradle/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
 
 	public void doGet (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
-        out.print("Hello BNSF from Gradle");
+        out.print("Hello Enterprise from Gradle");
         out.close();
     }
 }
--- src/main/java/org/gradle/MyClass.java
+++ src/main/java/org/gradle/MyClass.java
@@ -2,8 +2,11 @@ package org.gradle;
 
 public class MyClass {
 
-
     public boolean doSomething() throws Exception {
     	return true;
+    }
+    
+    public Boolean doBuggy(){
+    	return null;
     }
 }
